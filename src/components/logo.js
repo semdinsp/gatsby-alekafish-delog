df83ad0815d001d3a3089cdd0b1675841279d078
semdinsp <scott.sproule@gmail.com>
2022-05-18 23:10:13 -0700

    update to spelling of logo file

--- src/components/logo.js
+++ src/components/logo.js
@@ -1,6 +1,6 @@
 import React from "react"
 //import logo from "../images/alekalogo.png" // Tell webpack this JS file uses this image
-import logo from "../images/marenum2.jpg" // Tell webpack this JS file uses this image
+import logo from "../images/marenum2.jpeg" // Tell webpack this JS file uses this image
 function HeaderLogo() {
   // Import result is the URL of your image
   return <img src={logo} alt="Marenum Logo" placeholder="blurred" width={180} height={60}  />
